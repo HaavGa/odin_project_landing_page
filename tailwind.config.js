bc3479b7bd7fbb6197afe5c317df4763b0957a49
Håvard Garsrud <havard.garsrud@gmail.com>
2024-02-03 13:34:09 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1f2937",
+        main: "#f9faf8",
+        secondary: "#e5e7eb",
+        logo: "#f9faf8",
+        "main-blue": "#3882f6",
+        header: "#1f2937",
+        "quote-bg": "#e5e7eb",
+        quote: "#1f2937",
+      },
+    },
+  },
+  plugins: [],
+};
